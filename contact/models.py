970948efdfc5a8eda1c95b95c96e0faa4412cd27
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-01-25 14:57:52 -0300

    Add models and admin configurations for Category and Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = "Categories"
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models. TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
