1407cb940e9e6bf9154b0844f0591000bd6adffd
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-01-26 00:21:51 -0300

    Add contact detail view and template, update URL routing

--- contact/views.py
+++ contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 # Create your views here.
@@ -9,3 +9,11 @@ def index(request):
     }
 
     return render(request, "contact/index.html", context)
+
+def contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    context = {
+        "contact": single_contact
+    }
+
+    return render(request, "contact/contact.html", context)
