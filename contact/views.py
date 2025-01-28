aba5ede99ef1dacb3e6fb929a22125933e5d93a5
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-01-28 15:02:14 -0300

    Enhance contact index page with pagination, add search functionality, and update global templates with header and pagination partials

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by("-id")
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+   
     context = {
-        "contacts": contacts
+        "page_obj": page_obj,
+        "page_title": "Contatos"
     }
 
     return render(request, "contact/index.html", context)
@@ -13,7 +21,33 @@ def index(request):
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
     context = {
-        "contact": single_contact
+        "contact": single_contact,
+        "page_title": single_contact.first_name
     }
 
     return render(request, "contact/contact.html", context)
+
+def search(request):
+    search_value = request.GET.get("q", '').strip()
+    
+    if search_value == "":
+        return redirect("contact:index")
+    
+    contacts = Contact.objects.filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+    ).order_by("-id")
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "page_obj": page_obj,
+        "page_title": "Resultado da busca",
+        "search_value": search_value,
+    }
+    
+    return render(request, "contact/index.html", context=context)
